703f93057af5ad58ad91b90744e547679ae77fba
brendonstahl97 <brendonstahl97@gmail.com>
2020-10-13 20:29:23 -0400

    tried to debug restaurant.js

--- restaurants.js
+++ restaurants.js
@@ -1,26 +1,38 @@
 let form = document.getElementById("user-input");
-form.addEventListener('submit',e =>{
+var submitBtnEl = $(".submitBtn");
+
+
+submitBtnEl.on("click", e => {
   e.preventDefault();
-   let searchTerm = document.getElementById("ingredient").value; 
-   let cuisine = document.getElementById('dropdown').value;
-   var queryUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants&key=AIzaSyAz5S2li77P9Mh37AU2wN3bJ4_749FUZvY" + searchTerm + "&cuisine=" + cuisine;
-   
-   $.ajax({
-      url: queryUrl,
-      method: "GET",
-      dataType:"JSONP",
-      cache:false
+  // let searchTerm = document.getElementById("ingredient").value;
+  let cuisine = $(".dropMenu option:selected");
 
-  }).then( function(response) {
-      console.log(response);
+  var queryUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + cuisine + "restaurants&key=AIzaSyAz5S2li77P9Mh37AU2wN3bJ4_749FUZvY";
+  console.log(queryUrl);
+
+  var proxyurl = "https://cors-anywhere.herokuapp.com/";
+
+  console.log(proxyurl + queryUrl);
+
+  $.ajax({
+    url: proxyurl + queryUrl,
+    method: "GET",
+    // dataType: "JSONP",
+    // cache: false
+
+  }).then(function (response) {
+    console.log(response);
+
   })
-  const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
-const url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDxxU7miZ7Ya2z4gROnwiTK7BfdbdfNxcE&name=mexican";
-fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
-.then(response => response.json())
-.then(contents => console.log(contents))
-.catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+
+
+
+  // const url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyDxxU7miZ7Ya2z4gROnwiTK7BfdbdfNxcE&name=mexican";
+  // fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+  //   .then(response => response.json())
+  //   .then(contents => console.log(contents))
+  //   .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
 })
 
 
@@ -34,7 +46,7 @@ function getLocation() {
 
 function showPosition(position) {
   x.innerHTML = "Latitude: " + position.coords.latitude +
-  "<br>Longitude: " + position.coords.longitude;
+    "<br>Longitude: " + position.coords.longitude;
 }
 // https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyDxxU7miZ7Ya2z4gROnwiTK7BfdbdfNxcE&input=mexican&inputtype=textquery
 
