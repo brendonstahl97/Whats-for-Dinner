66cd577e75968acc98eab76307be00f682eda896
brendonstahl97 <brendonstahl97@gmail.com>
2020-10-15 20:03:25 -0400

    formatted generated cards

--- restaurants.js
+++ restaurants.js
@@ -8,12 +8,14 @@ function getLocation() {
   }
 }
 let userLocation = getLocation();
+
 function showPosition(position) {
- return 'location=' + position.coords.latitude + ',' + position.coords.longitude;
+  return 'location=' + position.coords.latitude + ',' + position.coords.longitude;
 }
 
 form.addEventListener('submit', e => {
-    e.preventDefault();
+
+  e.preventDefault();
   let searchTerm = document.getElementById('ingredient').value;
   let cuisine = document.getElementById('dropdown').value;
   console.log('cuisine:', cuisine);
@@ -45,41 +47,63 @@ form.addEventListener('submit', e => {
 });
 
 
-function createCards(response){
-  let container = document.getElementById("cardContainer");
-  let results = response.results;
-  results.forEach((result, index)=>{
-    if(index<5){
-      let card = document.createElement("div");
-      card.classList.add("restCard");
-      let image = document.createElement("img");
-      image.src=`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${result.photos[0].photo_reference}&key=AIzaSyAz5S2li77P9Mh37AU2wN3bJ4_749FUZvY`;        
-      card.appendChild(image);
-      let textContainer = document.createElement("div");
-      textContainer.classList.add("textContainer");
-      card.appendChild(textContainer);
-      let name = document.createElement("div");
-      let address = document.createElement("div");
-      let price = document.createElement("div");
-      let rating = document.createElement("div");
-      name.classList.add("name");
-      address.classList.add("address");
-      price.classList.add("price");
-      rating.classList.add("rating");
-      name.innerHTML=result.name;
-      address.innerHTML=result.formatted_address;
-      rating.innerHTML=result.rating;
+function createCards(response) {
+  let results = response.results.slice(0, 5);
 
-      for(let i=0; i<result.price_level; i++){
-        price.innerHTML+="$";
-      }
+  results.forEach(result => {
 
-      textContainer.appendChild(name);
-      textContainer.appendChild(address);
-      textContainer.appendChild(rating);
-      textContainer.appendChild(price);
-      container.appendChild(card);
+    var rowDiv = $("<div>");
+    rowDiv.addClass("row");
+
+    var colDiv = $("<div>");
+    colDiv.addClass("col s10 m8 offset-m1 l8");
+
+    var cardDiv = $("<div>");
+    cardDiv.addClass("card hoverable restaurant");
+
+    var cardImg = $("<div>");
+    cardImg.addClass("card-image cardImg");
+
+    var img = $("<img>");
+    img.attr("src", `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${result.photos[0].photo_reference}&key=AIzaSyAz5S2li77P9Mh37AU2wN3bJ4_749FUZvY`);
+
+    var cardSpan = $("<span>");
+    cardSpan.addClass("card-title");
+    cardSpan.text(result.name);
+
+    var cardContent = $("<div>");
+    cardContent.addClass("card-content");
+
+    var address = document.createElement("p");
+    var price = document.createElement("p");
+    var rating = document.createElement("p");
+
+    address.classList.add("address");
+    price.classList.add("price");
+    rating.classList.add("rating");
+
+    address.innerHTML = result.formatted_address;
+    rating.innerHTML = result.rating;
+
+    for (let i = 0; i < result.price_level; i++) {
+      price.innerHTML += "$";
     }
+
+    cardContent.append(address);
+    cardContent.append(rating);
+    cardContent.append(price);
+
+    cardImg.append(img);
+    cardImg.append(cardSpan);
+    cardDiv.append(cardImg);
+    cardDiv.append(cardContent);
+    colDiv.append(cardDiv);
+    rowDiv.append(colDiv);
+
+    $(".cardContainer").append(rowDiv);
+
+
+
   })
 
 }
